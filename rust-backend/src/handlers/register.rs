4ff05906465f721a0602ebde64e3b2cb43b9f237
Dawid33 <dawidso6467@gmail.com>
2021-05-14 12:54:15 +0100

    database update

--- rust-backend/src/handlers/register.rs
+++ rust-backend/src/handlers/register.rs
@@ -29,20 +29,25 @@ pub fn register_option(cors : rocket_cors::Guard<'_>) -> Responder<'_,status::Ac
 
 #[post("/register", data = "<register_form>")]
 pub fn register_post(register_form : Json<RegistrationForm>, conn : SQLiteConnection, cors : rocket_cors::Guard<'_>) ->  Responder<Json<RegistrationResponse>> {
+    //Create the 64 byte restoration key from random nums.
+    let restoration_key = "test";
+    let (key_salt,key_hash) = get_hash(restoration_key.as_bytes());
     let (salt,hash) = get_hash(register_form.username.as_bytes());
     
     let mut stmt = conn.prepare(
         r#"
-        INSERT INTO accounts (username, user_id, password, salt)
-        VALUES (?1,?2,?3,?4,?5); 
+        INSERT INTO accounts (username, user_id, password, salt,restoration_key,restoration_key_salt)
+        VALUES (?1,?2,?3,?4,?5,?6); 
         "#
     ).unwrap();
     let encoded_salt = HEXUPPER.encode(&salt);
     let encoded_hash = HEXUPPER.encode(&hash);
-    stmt.execute(&[&register_form.username,&0,&encoded_hash,&encoded_salt]).unwrap();
+    let encoded_key_salt = HEXUPPER.encode(&key_salt);
+    let encoded_key_hash = HEXUPPER.encode(&key_hash);
+    stmt.execute(&[&register_form.username,&0,&encoded_hash,&encoded_salt,&encoded_key_hash,&encoded_key_salt]).unwrap();
     let response = RegistrationResponse{
         accepted : true,
-        account_restoration_key : String::from(""),
+        account_restoration_key : String::from(restoration_key),
         cookie : String::from(""),
     };
     cors.responder(Json(response)) 
