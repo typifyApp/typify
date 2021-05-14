c61b47ec8fc6ded59b748e33fe4272abb854deec
Dawid33 <dawidsobczak@fastmail.com>
2021-05-14 13:55:09 +0100

    gen datatase script

---
+++ build_prod.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+build_dir="production-build"
+static_file_dir="public"
+binary="rust-backend"
+
+if [[ -d $build_dir ]]; then
+    echo ">> Removing old build in $build_dir"
+    rm -r $build_dir/$binary
+    echo "Done."
+else 
+    echo ">> Creating directory $build_dir"
+    mkdir $build_dir
+fi
+
+# Build frontend and backend
+echo ">> Building frontend..."
+(cd frontend; yarn build)
+echo ">> Building backend..."
+(cd rust-backend; cargo build --release)
+
+echo ">> Copying built files into $build_dir"
+cp rust-backend/target/release/rust-backend $build_dir/$binary
+cp rust-backend/Rocket.toml $build_dir/Rocket.toml
+cp rust-backend/gen_fresh_database.sh $build_dir/gen_fresh_database.sh
+cp -r frontend/build $build_dir/$static_file_dir
+chmod +x $build_dir/$binary
+
+# TEMP: just copy the debug one:
+cp rust-backend/typify.sqlite $build_dir/typify.sqlite
