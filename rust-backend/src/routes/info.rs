2cc00905db34ffbb532e7fc813a2fd109fa2a27b
Dawid33 <dawidsobczak@fastmail.com>
2021-06-03 21:02:54 +0100

    last commit before docker build

--- rust-backend/src/routes/info.rs
+++ rust-backend/src/routes/info.rs
@@ -1,11 +1,24 @@
 use rocket::http::{RawStr, uri::Query};
 use rocket_contrib::json::Json;
 use super::models::info::QueryResponse;
+use rocket_cors::Responder;
+use super::SQLiteConnection;
+
+#[options("/api/info?")]
+pub fn register_option(cors : rocket_cors::Guard<'_>) -> Responder<'_,rocket::response::status::Accepted<()>> {
+    cors.responder(rocket::response::status::Accepted(Some(())))
+}
 
 #[get("/api/info?<token>")]
-pub fn info(token : &RawStr) -> Json<QueryResponse> {
+pub fn info(token : &RawStr, conn : SQLiteConnection) -> Json<QueryResponse> {
     let token = token.url_decode().unwrap();
-    println!("{}", token);
+    let mut stmt = conn.prepare(
+        r#"
+        SELECT DISTINCT username, password, salt
+        FROM accounts
+        WHERE username=?1
+        "#
+    ).unwrap();
     let result = QueryResponse {
         name : String::from("Hello"),
     };
