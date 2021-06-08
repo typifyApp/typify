250ae3ae147205fabcc4cb7e4e9a089e05f3f549
Dawid33 <dawidsobczak@fastmail.com>
2021-06-08 19:14:56 +0100

    Cleaned up rust-backend directory.

--- rust-backend/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# let the executable
-if [[ $# != 1 ]]; then
-    echo "Incorrect amount of arguments, please specify the unprivledged user that will run the backend."
-else 
-    setcap cap_net_bind_service=ep $(pwd)/rust-backend
-    sudo -u $1 ./rust-backend
-fi
