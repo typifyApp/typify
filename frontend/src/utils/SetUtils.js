d4b2f69fd15699a228af23d0dc05053796222e22
Jim Ryan <jimmih15@gmail.com>
2021-06-30 21:03:14 +0100

    deleted old frontend

--- frontend/src/utils/SetUtils.js
+++
@@ -1,9 +0,0 @@
-const removeAll = (originalSet, toBeRemovedSet) => {
-  toBeRemovedSet.forEach(Set.prototype.delete, originalSet);
-};
-
-const SetUtils = {
-  removeAll,
-};
-
-export default SetUtils;
