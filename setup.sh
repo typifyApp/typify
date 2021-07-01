9d840a24c868b2b2ed8f3c3f8313a7329cc39c5d
Dawid33 <dawidsobczak@fastmail.com>
2021-07-01 12:22:08 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,25 +4,23 @@
 # Distro specific stuff
 if [ command -v pacman >& /dev/null ]; then
     sudo pacman -S openssl sqlite3
-else if [ command -v dnf >& /dev/null ]; then
-    
+else [ command -v dnf >& /dev/null ];
 fi
 # ============ Frontend ============
 
 # ============ Backend ============
 
 # Install rustup
-if [ ! command -v rustup ]; then
+if [ ! command -v rustup >& /dev/null ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     rustup toolchain install nightly
 fi
 
 echo "============Setting up frontend...============"
-{
+(
     cd frontend;
     echo "..."
-}
-
+)
 echo "============Setting up backend...============="
 # Distro with the pacman package manager
 (
